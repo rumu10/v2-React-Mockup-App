1c0e7e818434fff0ab40b31d44a0f9a3a1faeac2
sudipta <sudiptarumu@gmail.com>
2020-06-28 03:13:25 +0600

    Updates

---
+++ precache-manifest.3ce820165687125e35aa83b8df768f8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68f24d415d8812c02fcaef048546097",
+    "url": "/React-Mockup-App/index.html"
+  },
+  {
+    "revision": "76ec0295252382350e1f",
+    "url": "/React-Mockup-App/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "e6753b302c6e6c9ad5d8",
+    "url": "/React-Mockup-App/static/js/2.3bb02a56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Mockup-App/static/js/2.3bb02a56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76ec0295252382350e1f",
+    "url": "/React-Mockup-App/static/js/main.e19f2936.chunk.js"
+  },
+  {
+    "revision": "494b54eca4750d55709b",
+    "url": "/React-Mockup-App/static/js/runtime-main.bd98c40d.js"
+  }
+]);
